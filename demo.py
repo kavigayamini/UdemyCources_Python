9b61cdf4247ec7c21923ccb649405b38e019e202
kavigayamini <cst17025@std.uwu.ac.lk>
2021-09-29 11:29:08 +0530

    add oop commands

---
+++ demo.py
@@ -0,0 +1,23 @@
+print("Hello World..!!!")
+
+print(123456)
+print(-7846)
+
+print(5>8)
+print(10>5)
+
+name="Kavindya"
+name="Gayamini"
+print(name)
+
+
+number1=100
+number2=200
+number1=50
+total=number1+number2
+print(total)
+
+x="Hello"
+y="World"
+z=x+y
+print(z)
